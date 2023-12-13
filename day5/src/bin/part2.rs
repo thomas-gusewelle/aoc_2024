f54411f16c3730509b23c979b58d2349cf1e1ee2
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2023-12-13 10:37:13 -0600

    day 2 but rather slow

--- day5/src/bin/part2.rs
+++ day5/src/bin/part2.rs
@@ -1,3 +1,4 @@
+use indicatif::{ParallelProgressIterator, ProgressIterator};
 // use rayon::iter::IntoParallelRefMutIterator;
 use rayon::prelude::*;
 
@@ -54,7 +55,7 @@ impl Seed {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Map {
     destination: usize,
     source: usize,
@@ -77,9 +78,33 @@ impl Map {
     fn find_location(&self, seed: usize) -> Option<usize> {
         if seed >= self.source && seed <= self.source + self.range - 1 {
             let length_between = seed - self.source;
+
             return Some(self.destination + length_between);
         }
         return None;
+    }
+}
+
+
+#[derive(Debug, Clone )]
+struct SeedMaps {
+    maps: Vec<Map>
+}
+
+impl SeedMaps {
+    fn new(maps: Vec<Map>) -> SeedMaps {
+SeedMaps { maps }
+    }
+    fn translate(&self, seed: usize) -> usize {
+        let conversions: Vec<Option<usize>> = self.maps.iter().map(|x| x.find_location(seed.clone())).collect();
+        if let Some(matched) = conversions.into_iter().find(|x| match x {
+            Some(_) => return true,
+            None => return false
+        }) {
+            return matched.unwrap();
+        }
+        return seed;
+
     }
 }
 fn solution(input: &str) -> usize {
@@ -93,46 +118,54 @@ fn solution(input: &str) -> usize {
         .map(|x| x.parse().unwrap())
         .collect();
 
-    let seeds: Vec<usize> = seeds_vec
+    let mut locations: Vec<usize> = seeds_vec
         .windows(2)
         .step_by(2)
+        .progress()
         .flat_map(|x| x[0]..(x[0] + x[1]))
         .collect();
 
-    let mut locations: Vec<usize> = seeds.clone();
-
-    let mut all_maps: Vec<&Vec<Map>> = vec![];
+    let mut all_maps: Vec<SeedMaps> = vec![];
     let ss_map: Vec<Map> = maps[1].lines().map(|x| Map::new(x)).collect();
-    all_maps.push(&ss_map);
+    all_maps.push(SeedMaps::new(ss_map));
     let sf_map: Vec<Map> = maps[2].lines().map(|x| Map::new(x)).collect();
-    all_maps.push(&sf_map);
+    all_maps.push(SeedMaps::new(sf_map));
     let fw_map: Vec<Map> = maps[3].lines().map(|x| Map::new(x)).collect();
-    all_maps.push(&fw_map);
+    all_maps.push(SeedMaps::new(fw_map));
     let wl_map: Vec<Map> = maps[4].lines().map(|x| Map::new(x)).collect();
-    all_maps.push(&wl_map);
+    all_maps.push(SeedMaps::new(wl_map));
     let lt_map: Vec<Map> = maps[5].lines().map(|x| Map::new(x)).collect();
-    all_maps.push(&lt_map);
+    all_maps.push(SeedMaps::new(lt_map));
     let th_map: Vec<Map> = maps[6].lines().map(|x| Map::new(x)).collect();
-    all_maps.push(&th_map);
+    all_maps.push(SeedMaps::new(th_map));
     let hl_map: Vec<Map> = maps[7].lines().map(|x| Map::new(x)).collect();
-    all_maps.push(&hl_map);
+    all_maps.push(SeedMaps::new(hl_map));
 
-    for map in all_maps.into_iter() {
-        locations.par_iter_mut().for_each(|seed| {
-            let conversions: Vec<Option<usize>> = map
-                .into_iter()
-                .map(|x| x.find_location(seed.clone()))
-                .collect();
-            if let Some(matched) = conversions.into_iter().find(|x| match x {
-                Some(_) => return true,
-                _ => return false,
-            }) {
-                *seed = matched.unwrap();
-            }
-        })
-    }
 
-    locations.into_iter().min().unwrap()
+    let locations: Vec<usize> = locations.par_iter().progress().map(|seed| {
+        all_maps
+            .iter()
+            .fold(*seed, |seed, maps| {
+                maps.translate(seed)
+            })
+    }).collect();
+
+    // for map in all_maps.into_iter().progress() {
+    //     locations.par_iter_mut().progress().for_each(|seed| {
+    //         let conversions: Vec<Option<usize>> = map
+    //             .into_iter()
+    //             .map(|x| x.find_location(seed.clone()))
+    //             .collect();
+    //         if let Some(matched) = conversions.into_iter().find(|x| match x {
+    //             Some(_) => return true,
+    //             _ => return false,
+    //         }) {
+    //             *seed = matched.unwrap();
+    //         }
+    //     })
+    // }
+
+    *locations.par_iter().progress().min().unwrap()
 }
 
 #[cfg(test)]
